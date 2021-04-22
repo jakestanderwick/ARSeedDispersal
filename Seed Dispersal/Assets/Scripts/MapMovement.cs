86fe3914c8ed8a7ea519de4775c6fb72e001c5e9
jakestanderwick <jstan710@hotmail.com>
2021-04-22 16:48:54 -0600

    Map Functionality
    Fixed pulling up map, need progress markers on map. No implementation of click and drag

---
+++ Seed Dispersal/Assets/Scripts/MapMovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapMovement : PlacementController
+{
+    bool mapDown = true;
+    public bool[] setOn = new bool[5];
+    bool test = false;
+    public Button mapSpriteUp;
+    public Button mapSpriteDown;
+    public GameObject step1, step2, step3, step4, step5, step6;
+    void Start()
+    {
+        mapSpriteUp.gameObject.SetActive(false);
+    }
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+    void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if(Input.touchCount > 0)
+        {
+            Debug.Log(Input.touchCount);
+            Touch touch = Input.GetTouch(0);
+            touchPosition = touch.position;
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamera.ScreenPointToRay(touchPosition);
+                if(Physics.Raycast(ray, out hitObject))
+                {
+                    if(hitObject.transform.name.Contains("Check1"))
+                    {
+                        mapSpriteDown.gameObject.SetActive(false);
+                        mapSpriteUp.gameObject.SetActive(true);
+                    }
+                    onTouchHold = true;
+                }
+            }
+            if(touch.phase == TouchPhase.Moved)
+            {
+                touchPosition = touch.position;
+            }
+            if(touch.phase == TouchPhase.Ended)
+            {
+                onTouchHold = false;
+            }
+            if(onTouchHold)
+            { 
+                if(hitObject.collider != null)
+                {
+                    hitObject.transform.position = touch.position;
+                }
+            }
+        }
+    }
+
+    public void MapController()
+    {
+        if(mapDown == true)
+        {
+            //mapSprite.transform.position = new Vector3 (0f, -440f, 3f);
+            mapSpriteDown.gameObject.SetActive(false);
+            mapSpriteUp.gameObject.SetActive(true);
+            //step1.gameObject.SetActive(true);
+            if(test == true)
+            {
+                step1.gameObject.SetActive(true);
+            }
+            mapDown = false;
+        }
+        else if(mapDown == false)
+        {
+            mapSpriteDown.gameObject.SetActive(true);
+            mapSpriteUp.gameObject.SetActive(false);
+            // step1.gameObject.SetActive(false);
+            // step2.gameObject.SetActive(false);
+            // step3.gameObject.SetActive(false);
+            // step4.gameObject.SetActive(false);
+            // step5.gameObject.SetActive(false);
+            // step6.gameObject.SetActive(false);
+            // float fx = mapSprite.transform.position.x + 10;
+            // float fy = mapSprite.transform.position.y + 10;
+            // float fz = mapSprite.transform.position.z + 10;
+            // mapSprite.transform.position = new Vector3(fx, fy, fz);
+            //mapSprite.transform.rotation = new Quaternion(0f,180f,0f,0f);
+            mapDown = true;
+        }
+    }
+}
--- Seed Dispersal/Assets/Scripts/PlacementController.cs
+++ Seed Dispersal/Assets/Scripts/PlacementController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(ARRaycastManager))]
 
@@ -14,14 +15,21 @@ public class PlacementController : MonoBehaviour
     private GameObject instructions;
 
     [SerializeField]
-    private Camera arCamera;
+    public Camera arCamera;
 
     [SerializeField]
     private ARRaycastManager aRRaycastManager;
-    private bool onTouchHold = false;
+    [SerializeField]
+    public RaycastHit hitObject;
+    public bool onTouchHold = false;
     public bool onMapTouch = false;
     bool mapLowered = true;
     public GameObject SeedSearchPlane;
+    public GameObject debugDemo;
+    public Button ButtonUp;
+    //public GameObject s1, s2, s3, s4, s5, s6;
+    private GameObject placedObject;
+    
 
     private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
@@ -37,6 +45,15 @@ public class PlacementController : MonoBehaviour
 
     private ARRaycastManager arRaycastManager;
 
+    void Start()
+    {
+        // setOn[0] = false;
+        // setOn[1] = false;
+        // setOn[2] = false;
+        // setOn[3] = false;
+        // setOn[4] = false;
+        // setOn[5] = false;
+    }
     void Awake()
     {
         arRaycastManager = GetComponent<ARRaycastManager>();
@@ -55,7 +72,7 @@ public class PlacementController : MonoBehaviour
 
     void Update()
     {
-        
+        //arCamera.transform.position = SeedSearchPlane.transform.position;
         if(!TryGetTouchPosition(out Vector2 touchPosition))
             return;
         
@@ -65,7 +82,11 @@ public class PlacementController : MonoBehaviour
 
         //     Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
         // }
+        // float fx = arCamera.transform.position.x - 2;
+        // float fy = arCamera.transform.position.y - 2;
+        // float fz = arCamera.transform.position.z - 2;
 
+        // SeedSearchPlane.transform.position = new Vector3(fx, fy, fz);
 
         if(Input.touchCount > 0)
         {
@@ -76,27 +97,101 @@ public class PlacementController : MonoBehaviour
             if(touch.phase == TouchPhase.Began)
             {
                 Ray ray = arCamera.ScreenPointToRay(touchPosition);
-                RaycastHit hitObject;
+                //RaycastHit hitObject;
                 if(Physics.Raycast(ray, out hitObject))
                 {
                     if(hitObject.transform.name.Contains("SeedSearchMap"))
                     {
                         //onTouchHold = true;
                         //onMapTouch = true;
-                        ManageMap();
+                        //ManageMap();
+                        //float fx = arCamera.transform.position.x - 2;
+                        //float fy = arCamera.transform.position.y - 2;
+                        //float fz = arCamera.transform.position.z - 2;
+                        // if(mapLowered == true)
+                        // {
+
+                        //     SeedSearchPlane.transform.position = new Vector3(0f, -0.71f, 3.65f);
+                        //     //SeedSearchPlane.transform.position = new Vector3(arCamera.transform.position.x, 1f, arCamera.transform.position.z);
+                        //     SeedSearchPlane.transform.rotation = new Quaternion(75.881f, 181.165f, 0f, 0f);
+                        //     mapLowered = false;
+                        // }
+                        // else if(mapLowered == false)
+                        // {
+                        //     SeedSearchPlane.transform.position = new Vector3(-0.043f, -1.836f, 2.207f);
+                        //     //SeedSearchPlane.transform.position = new Vector3(arCamera.transform.position.x, 2f, arCamera.transform.position.z);
+                        //     SeedSearchPlane.transform.rotation = new Quaternion(24.304f, 181.165f, 0f, 0f);
+                        //     mapLowered = true;
+                        // }
                         Debug.Log("Made it to map switching");
                     }
+                    // if(hitObject.transform.name.Contains("Check1"))
+                    // {
+                    //     //s1.gameObject.SetActive(true);
+                    //     setOn[0] = true;
+                    // }
+                    else{
+                        onTouchHold = true;
+                        //arCamera.transform.position = SeedSearchPlane.transform.position;
+                        //onMapTouch = true;
+                        //ManageMap();
+                        //Debug.Log("Made it to map switching");
+                    }
+                    
+                    
                 }
+                // if((Physics.Raycast(ray, out hitObject) && (hitObject.collider.tag == "Draggable"))
+                // {
+                //     toDrag
+                // }
+            }
+            if(touch.phase == TouchPhase.Moved)
+            {
+                touchPosition = touch.position;
             }
             if(touch.phase == TouchPhase.Ended)
             {
                 onTouchHold = false;
             }
+
             // if(onTouchHold)
             // {
             //     //placedObject.transform.position = hitPose.position;
             //     //same with
             // }
+            if(onTouchHold)
+            {
+                
+                if(hitObject.collider != null)
+                {
+                    //Vector3 pos = touchPosition;
+
+                    ////hitObject.transform.position = touch.position;
+
+
+                    //hitObject.transform.position = new Vector3(1f,1f,8f);
+                    //Debug.Log(hitObject.ToString());
+                    //hitObject.transform.position = Camera.main.ScreenToWorldPoint(touchPosition);
+
+                    // for (int i = 0; i < Input.touchCount; i++)
+                    // {
+                    //     if (Input.GetTouch(i).phase == TouchPhase.Began)
+                    //     {
+                    //         //Vector3 p = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                    //         hitObject.transform.position = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                    //     }
+                    // }
+                }
+            }
+            if(arRaycastManager.Raycast(touchPosition, hits))
+            {
+                Pose hitPose = hits[0].pose;
+
+                if(onTouchHold)
+                {
+                    placedObject.transform.position = hitPose.position;
+                }
+            }
         }
 
     }
@@ -106,35 +201,36 @@ public class PlacementController : MonoBehaviour
 
     }
 
-    void ManageMap()
-    {
-        //if(onMapTouch == true)
-        //{
-            if(mapLowered == true)
-            {
-                BringUpMap();
-            }
-            if(mapLowered == false)
-            {
-                LowerMap();
-            }
-            onMapTouch = false;
-        //}
+    // void ManageMap()
+    // {
+    //     //if(onMapTouch == true)
+    //     //{
+    //         if(mapLowered == true)
+    //         {
+    //             BringUpMap();
+    //         }
+    //         if(mapLowered == false)
+    //         {
+    //             LowerMap();
+    //         }
+    //         //onMapTouch = false;
+    //     //}
         
-    }
-    void BringUpMap()
-    {
-        SeedSearchPlane.transform.position = new Vector3(0f, -0.78f, 3.83f);
-        SeedSearchPlane.transform.rotation = new Quaternion(75.829f, 181.165f, 0f, 0f);
-        mapLowered = false;
-    }
+    // }
+    // void BringUpMap()
+    // {
+    //     SeedSearchPlane.transform.position = new Vector3(0f, 0.38f, 6.09f);
+    //     SeedSearchPlane.transform.rotation = new Quaternion(70.352f, 181.165f, 0f, 0f);
+    //     mapLowered = false;
 
-    void LowerMap()
-    {
-        SeedSearchPlane.transform.position = new Vector3(-0.043f, -1.836f, 2.207f);
-        SeedSearchPlane.transform.rotation = new Quaternion(24.304f, 181.165f, 0f, 0f);
-        mapLowered = true;
-    }
+    // }
+
+    // void LowerMap()
+    // {
+    //     SeedSearchPlane.transform.position = new Vector3(-0.043f, -1.036f, 5.297f);
+    //     SeedSearchPlane.transform.rotation = new Quaternion(24.304f, 181.165f, 0f, 0f);
+    //     mapLowered = true;
+    // }
 
     
 }
